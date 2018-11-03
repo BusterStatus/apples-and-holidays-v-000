12ef8986a56ccef789bf8eb1d3f18aac78a3c015
matthew.serre <matthew.serre@gmail.com>
2018-11-03 01:09:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
         element.capitalize!
       end
       joined_array = split_array.join(" ")
-      puts "#{joined_array}:"
+      puts " #{joined_array}:"
       supplies.each do |supply|
         puts supply.to_s
       end
